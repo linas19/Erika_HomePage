b4b563c1e7c50ce8136b70fbe865798bb530777f
Linas Jonika <linas.jonika1@gmail.com>
2021-04-18 19:39:53 +0300

    added email function, still need to change form

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,40 @@ import styles from '../styles/Home.module.scss'
 import Image from 'next/image.js'
 import { faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { useState } from 'react'
 import Link from 'next/link'
 
 export default function Home() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Sending')
+    let data = {
+      name,
+      email,
+      message
+    }
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((res) => {
+      console.log('Response received')
+      if (res.status === 200) {
+        console.log('Response succeeded!')
+        setSubmitted(true)
+        setName('')
+        setEmail('')
+        setBody('')
+      }
+    })
+  }
   return (
     
     <div className={styles.container}>
@@ -215,6 +245,23 @@ export default function Home() {
               </Link>
             </div>
           </div>
+        </div>
+        <div className={styles.container}>
+          < form className={styles.main} >
+          < formGroup className={styles.inputGroup} >
+            < label htmlFor='name'>Name</label>
+            < input type='text' onChange={(e)=>{setName(e.target.value)}} name='name' className={styles.inputField} />  
+          </formGroup>
+          < formGroup className={styles.inputGroup} >
+            < label htmlFor='email'>Email</label>
+            < input type='email' onChange={(e)=>{setEmail(e.target.value)}} name='email' className={styles.inputField} />
+          </formGroup>
+          < formGroup className={styles.inputGroup} >
+            < label htmlFor='message'>Message</label>
+            < input type='text' onChange={(e)=>{setMessage(e.target.value)}} name='message' className={styles.inputField} />
+          </formGroup>
+          < input type='submit' onClick={(e)=>{handleSubmit(e)}}/>
+          </form >
         </div>
       </body>
       <main className={styles.main}>
