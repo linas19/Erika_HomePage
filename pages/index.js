9498ded628fb1b7d27736696c41324fbf9355e53
Linas Jonika <linas.jonika1@gmail.com>
2021-04-18 15:17:02 +0300

    fixed third page bottom

--- pages/index.js
+++ pages/index.js
@@ -111,12 +111,12 @@ export default function Home() {
             </div>
           </div>
           <div className={styles.thirdPageBottom}>
-            <div>
+            <div className={styles.thirdPageBottomText}>
               <p>I’ve been really impressed in my time working with Erika in her role as one of the leading developers in the Eurosport team.</p>
               <p>I find Erika astute, approachable and engaging - she takes the time and effort to walk through implications and options for decisions we need to make and is a really solid trusted sounding board on technical feasibility and approaches to requirements needs.</p>
               <p>On top of being a really good developer and really hard working, Erika is a joy to work with from a business and stakeholder engagement perspective and I hope to work with her again in the future.</p>
             </div>
-            <div>Alex Brown, Senior Product Manager at Eurosport</div>
+            <div className={styles.thirdPageBottomAuthor}>Alex Brown, Senior Product Manager at Eurosport</div>
           </div>
         </div>
         <div className={styles.thirdPage}>
