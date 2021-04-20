06ea9f575cb2fa5940164881f9254c3a8601cdb6
Linas Jonika <linas.jonika1@gmail.com>
2021-04-20 15:19:34 +0300

    fixed first section

--- pages/index.js
+++ pages/index.js
@@ -59,8 +59,11 @@ export default function Home() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
       <body>
-        <NavBar />
-        <FirstPage />
+        <section className={styles.firstSection}>
+          <NavBar />
+          <FirstPage />
+        </section>
+
         {/* <SecondPage />
         <ThirdPage /> */}
         {/* <div className={styles.fourthPage}>
