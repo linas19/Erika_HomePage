341a9461c9bab287f0baea685c1bc14cf58bd4b0
erikajon <hello@jonika.io>
2021-04-20 18:55:54 +0100

    Basic setup of contentful

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,11 @@
 import styles from "../../styles/Blog.module.scss";
 import Head from "next/head";
 import Link from "next/link";
+import Markdown from "react-markdown";
+import gfm from "remark-gfm";
+import { fetchPosts } from '../../data/posts';
 
-export default function FirstPost() {
+export default function Blog({ posts }) {
   return (
     <div>
       <Head>
@@ -16,7 +19,7 @@ export default function FirstPost() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
       </Head>
-      <body>
+      <div>
         <div className={styles.topLine}></div>
         <div className={styles.navBar}>
           <div className={styles.navBarLeft}>
@@ -30,14 +33,41 @@ export default function FirstPost() {
               <Link href="https://www.instagram.com/">ABOUT</Link>
             </button>
             <button className={styles.navBarRightButton} id={styles.blogButton}>
-              <Link href="/blog/blog">BLOG</Link>
+              <Link href="/blog">BLOG</Link>
             </button>
             <button className={styles.navBarRightButton}>
-              <Link href="../">GET IN TOUCH</Link>
+              <Link href="/">GET IN TOUCH</Link>
             </button>
           </div>
         </div>
-      </body>
+
+        <div>
+          {posts.map((p) => {
+            console.log('post', p);
+            return (
+              <div key={p.title}>
+                {/* <div>{p.title}</div> */}
+                <div>
+                  <Markdown remarkPlugins={[gfm]} children={p.body} />
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetchPosts()
+  const posts = await res.map((p) => {
+    return p.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
